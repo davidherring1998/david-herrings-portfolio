7caaced009a3350e6d699d8579e68eefda709c4f
David Herring <davidherring@Davids-MBP.home>
2023-04-16 00:06:30 -0500

    fixing grammar in the about me page

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -14,12 +14,12 @@ function Section() {
           Hello! My name is David Herring and I discovered my passion for
           information technology early on through many years in the music
           production industry. After operating a recording studio through my
-          early 20s, I have found that I thrive in a goal-driven,
+          early 20s, I have found that I thrive in goal-driven,
           problem-solving-oriented work. This realization led me down an
           information technology path consisting of front-end and back-end web
-          development and with that, many customer service skills. I recently
-          graduated from a Vanderbilt coding boot camp and am now currently in
-          the process of enrolling into a college for computer science.
+          development and many customer service skills. I recently graduated
+          from a Vanderbilt coding boot camp and am now currently in the process
+          of enrolling in a college for computer science.
         </p>
       </div>
     </div>
